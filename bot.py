5b647d72083bc91c147d54cb71ac70beab9aabde
Benji Tobias <benjitobias@gmail.com>
2020-08-10 10:38:18 +0300

    initial commit

---
+++ bot.py
@@ -0,0 +1,17 @@
+import requests
+
+with open("SECRET_API_TOKEN", "r") as token_file:
+    secret_api_token = token_file.read()
+
+def telegram_bot_sendtext(bot_message):
+    bot_token = secret_api_token
+    bot_chatID = '13225322'
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+
+    response = requests.get(send_text)
+
+    return response.json()
+
+test = telegram_bot_sendtext("Test shit")
+print(test)
+
