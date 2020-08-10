4d653db0069e24150f6233e4793e5c37db37a77a
Benji Tobias <benjitobias@gmail.com>
2020-08-10 10:40:31 +0300

    initial commit - bugfix

--- bot.py
+++ bot.py
@@ -1,13 +1,15 @@
 import requests
 
 with open("SECRET_API_TOKEN", "r") as token_file:
-    secret_api_token = token_file.read()
+    secret_api_token = token_file.read().strip()
+print("Secret_token: " + secret_api_token)
 
 def telegram_bot_sendtext(bot_message):
     bot_token = secret_api_token
     bot_chatID = '13225322'
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
 
+    print("Request: " + send_text)
     response = requests.get(send_text)
 
     return response.json()
